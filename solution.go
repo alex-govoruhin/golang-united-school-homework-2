57ac6667195d6304173482ec68e2e50a302562f4
alex.govoruhin <alex.govoruhin@gmail.com>
2022-02-17 22:57:49 +0500

    lesson-01

--- solution.go
+++ solution.go
@@ -1,5 +1,14 @@
 package square
 
+import (
+	"math"
+)
+
+type sidesType int
+
+const SidesCircle sidesType = 0;
+const SidesTriangle sidesType = 3;
+const SidesSquare sidesType = 4;
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +17,32 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum sidesType) float64 {
+	var square float64
+
+	switch sidesNum {
+		case 0: square = calcCircle(sideLen)
+		case 3: square = calcTringle(sideLen)
+		case 4: square = calcSquare(sideLen)
+	}
+
+	return square
+}
+
+func calcTringle(sideLen float64) (square float64) {
+	square = (math.Pow(sideLen, 2) * math.Sqrt(3)) / 4
+
+	return
+}
+
+func calcSquare(sideLen float64) (square float64) {
+	square = math.Pow(sideLen, 2)
+
+	return
+}
+
+func calcCircle(sideLen float64) (square float64) {
+	square = math.Pi * math.Pow(sideLen, 2)
+
+	return
 }
